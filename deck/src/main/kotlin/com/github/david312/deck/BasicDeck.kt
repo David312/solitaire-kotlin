2a270d75100dd24948fe3742509a1a03f94c23e7
David312 <davidhs312yt@gmail.com>
2021-08-16 22:18:35 +0200

    Basic project structure

---
+++ deck/src/main/kotlin/com/github/david312/deck/BasicDeck.kt
@@ -0,0 +1,4 @@
+package com.github.david312.deck
+
+open class BasicDeck {
+}
---
+++ deck/src/test/kotlin/com/github/david312/deck/BasicDeckTest.kt
@@ -0,0 +1,5 @@
+package com.github.david312.deck
+
+class BasicDeckTest {
+    private lateinit var basicDeck: BasicDeck
+}
